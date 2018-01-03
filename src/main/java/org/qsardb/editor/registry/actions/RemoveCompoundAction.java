29e395f90f508228c9c6df92ce27341172900b63
Sulev Sild <sulev.sild@ut.ee>
2018-01-04 00:21:08 +0200

    Remove references when deleting compounds

--- src/main/java/org/qsardb/editor/registry/actions/RemoveCompoundAction.java
+++ src/main/java/org/qsardb/editor/registry/actions/RemoveCompoundAction.java
@@ -8,12 +8,18 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import org.qsardb.cargo.map.MapCargo;
+import org.qsardb.cargo.map.ReferencesCargo;
 import org.qsardb.cargo.map.StringFormat;
 import org.qsardb.cargo.map.ValuesCargo;
 import org.qsardb.editor.common.QdbContext;
 import org.qsardb.editor.common.Utils;
 import org.qsardb.editor.events.CompoundEvent;
-import org.qsardb.model.*;
+import org.qsardb.model.ByteArrayPayload;
+import org.qsardb.model.Compound;
+import org.qsardb.model.Parameter;
+import org.qsardb.model.ParameterRegistry;
+import org.qsardb.model.Qdb;
 
 public class RemoveCompoundAction extends RemoveContainerAction<Compound>{
 	public RemoveCompoundAction(QdbContext context) {
@@ -24,9 +30,9 @@ public class RemoveCompoundAction extends RemoveContainerAction<Compound>{
 	protected CompoundEvent remove(ActionEvent e, Compound compound) {
 		try {
 			Qdb qdb = compound.getQdb();
-			removeValues(qdb.getPropertyRegistry(), compound);
-			removeValues(qdb.getDescriptorRegistry(), compound);
-			removeValues(qdb.getPredictionRegistry(), compound);
+			removeCompoundData(qdb.getPropertyRegistry(), compound);
+			removeCompoundData(qdb.getDescriptorRegistry(), compound);
+			removeCompoundData(qdb.getPredictionRegistry(), compound);
 			compound.getRegistry().remove(compound);
 			return new CompoundEvent(this, CompoundEvent.Type.Remove, compound);
 		} catch (IOException ex) {
@@ -35,32 +41,26 @@ public class RemoveCompoundAction extends RemoveContainerAction<Compound>{
 		return null;
 	}
 
-	private void removeValues(ParameterRegistry registry, Compound compound) throws IOException {
-		for (Object o: registry) {
-			ValuesCargo values = getValuesCargo((Container)o);
-			if (values == null) {
-				continue;
-			}
-			Map<String, String> map = new LinkedHashMap<String, String>(values.loadStringMap());
-			if (map.remove(compound.getId()) != null) {
-				ByteArrayOutputStream os = new ByteArrayOutputStream();
-				values.formatMap(map, new StringFormat(), os);
-				values.setPayload(new ByteArrayPayload(os.toByteArray()));
-			}
+	private void removeCompoundData(ParameterRegistry<?,?> registry, Compound compound) throws IOException {
+		for (Parameter<?,?> parameter: registry) {
+			removeCompoundData(parameter, compound, ValuesCargo.class);
+			removeCompoundData(parameter, compound, ReferencesCargo.class);
 		}
 	}
 
-	private ValuesCargo getValuesCargo(Container container) {
-		if (container.hasCargo(ValuesCargo.class)) {
-			if (container instanceof Property) {
-				return ((Property)container).getCargo(ValuesCargo.class);
-			} else if (container instanceof Descriptor) {
-				return ((Descriptor)container).getCargo(ValuesCargo.class);
-			} else if (container instanceof Prediction) {
-				return ((Prediction)container).getCargo(ValuesCargo.class);
-			}
-			throw new IllegalArgumentException();
+	private void removeCompoundData(Parameter<?,?> parameter, Compound compound, Class<? extends MapCargo> clazz) throws IOException {
+
+		if (!parameter.hasCargo(clazz)) {
+			return;
 		}
-		return null;
+
+		MapCargo values = parameter.getCargo(clazz);
+		Map<String, String> map = new LinkedHashMap<>(values.loadStringMap());
+		
+		if (map.remove(compound.getId()) != null) {
+			ByteArrayOutputStream os = new ByteArrayOutputStream();
+			values.formatMap(map, new StringFormat(), os);
+			values.setPayload(new ByteArrayPayload(os.toByteArray()));
+		}
 	}
 }
